4c9fd7c80a7a715b994d1a9225e908c67d062c03
woaomo <609429505@qq.com>
2014-12-02 17:44:18 +0800

    修改上拉

--- Hubanghu/Hubanghu/TwoViewController/SecondViewController.m
+++ Hubanghu/Hubanghu/TwoViewController/SecondViewController.m
@@ -220,7 +220,7 @@ typedef enum : NSUInteger {
     
 
         [weakself.showWorkerListTableView addInfiniteScrollingWithActionHandler:^{
-            if(self.workersArray.count==20)
+            if(self.workersArray.count%20==0 &&self.workersArray.count>0)
             {
                 int64_t delayInSeconds = 2.0;
                 dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
